077431abfacc45e67a6e97763c5d6eb895950388
Bülent Öner <boe1iz@gmail.com>
2015-01-09 17:58:45 +0200

    Commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SAP.Middleware.Connector;
+using System.Data;
 
 namespace ConsoleApplication1
 {
@@ -30,6 +31,95 @@ namespace ConsoleApplication1
                 RfcDestination rfcDest = RfcDestinationManager.GetDestination("P78");
                 RfcRepository repo = rfcDest.Repository;
                 Console.WriteLine("Connected!");
+
+                IRfcFunction COPAQuery = repo.CreateFunction("BAPI_COPAQUERY_GETCOST_ACTDATA");
+                COPAQuery.SetValue("OPERATINGCONCERN", "8290");
+                COPAQuery.SetValue("CURRENCYTYPE", "B0");
+                COPAQuery.SetValue("MAXRECORDS", "99999999");
+
+                IRfcTable SELECTION = COPAQuery.GetTable("SELECTION");
+
+                SELECTION.Append();
+                SELECTION.SetValue("FIELDNAME", "PERIO");
+                SELECTION.SetValue("SIGN", "I");
+                SELECTION.SetValue("OPTION", "BT");
+                SELECTION.SetValue("LOW", "2015001");
+                SELECTION.SetValue("HIGH", "2015001");
+
+                SELECTION.Append();
+                SELECTION.SetValue("FIELDNAME", "VRGAR");
+                SELECTION.SetValue("SIGN", "I");
+                SELECTION.SetValue("OPTION", "EQ");
+                SELECTION.SetValue("LOW", "F");
+                SELECTION.SetValue("HIGH", "F");
+
+                SELECTION.Append();
+                SELECTION.SetValue("FIELDNAME", "VRGAR");
+                SELECTION.SetValue("SIGN", "I");
+                SELECTION.SetValue("OPTION", "EQ");
+                SELECTION.SetValue("LOW", "F");
+                SELECTION.SetValue("HIGH", "F");
+
+                IRfcTable SELECTEDFIELDS = COPAQuery.GetTable("SELECTEDFIELDS");
+
+                SELECTEDFIELDS.Append();
+                SELECTEDFIELDS.SetValue("FIELDNAME", "BELNR");
+                SELECTEDFIELDS.Append();
+                SELECTEDFIELDS.SetValue("FIELDNAME", "PERIO");
+                SELECTEDFIELDS.Append();
+                SELECTEDFIELDS.SetValue("FIELDNAME", "PAPH3");
+
+                IRfcTable RESULTDATA = COPAQuery.GetTable("RESULTDATA");
+                IRfcTable RETURN = COPAQuery.GetTable("RETURN");
+
+                COPAQuery.Invoke(rfcDest);
+
+                DataTable rstData = new DataTable();
+
+                if (RESULTDATA.RowCount > 0)
+                {
+                    for (int tempFname = 0; tempFname < SELECTEDFIELDS.RowCount; tempFname++)
+                    {
+                        SELECTEDFIELDS.CurrentIndex = tempFname;
+                        rstData.Columns.Add(SELECTEDFIELDS.GetString("FIELDNAME"), typeof(string));
+                    }
+                }
+
+                string kkk = "";
+
+                for (int DATAPTR = 1; DATAPTR < RESULTDATA.RowCount; DATAPTR++)
+                {
+                    string[] ttt = new string[SELECTEDFIELDS.RowCount];
+                    for (int DATAPTR2 = 0; DATAPTR2 < SELECTEDFIELDS.RowCount; DATAPTR2++)
+                    {
+                        RESULTDATA.CurrentIndex = DATAPTR - 1;
+                        kkk = RESULTDATA.GetString("VALUE").Trim().Replace(".", ",");
+                        ttt[DATAPTR2] = ((kkk.Substring(kkk.Length - 1) == "-" ? "-" + kkk.Replace("-", "") : kkk));
+                        if ((DATAPTR % (SELECTEDFIELDS.RowCount) == 0))
+                        {
+                            break;
+                        }
+                        DATAPTR++;
+                    }
+                    rstData.Rows.Add(ttt);
+                    ttt = null;
+                }
+
+                rstData.DefaultView.Sort = "BELNR";
+
+                foreach (DataRow row in rstData.Rows)
+                {
+                    foreach (var item in row.ItemArray)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+                Console.WriteLine("Read COPA - " + (RESULTDATA.RowCount / SELECTEDFIELDS.RowCount).ToString() + " row(s) read");
+
+
+
+
             }
             catch (RfcLogonException ex)
             {
